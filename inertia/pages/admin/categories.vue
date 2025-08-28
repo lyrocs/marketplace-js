2a2cd08ab981082381a15a9f924b2d1f2c7968f3
Lyrocs <cjuanole@gmail.com>
2025-08-28 10:16:31 +0200

    Implement Delete on GenericTable

--- inertia/pages/admin/categories.vue
+++ inertia/pages/admin/categories.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
 import { router } from '@inertiajs/vue3'
 import AdminLayout from '~/layouts/AdminLayout.vue'
 import GenericTable from '../../components/GenericTable.vue'
@@ -7,38 +6,29 @@ import CategoryDto from '#dtos/category'
 defineOptions({ layout: AdminLayout })
 
 const props = defineProps<{ categories: CategoryDto[] }>()
-const newCategoryName = ref('')
-const newCategoryParentId = ref<number | null>(null)
-const newCategorySpecsTypes = ref<any>({})
 
-function createCategory({ name, parentId, specsTypes }: { name: string, parentId: number | null, specsTypes: any }) {
-    router.post('/admin/categories', { name, parentId, specsTypes }, {
-        onSuccess: () => {
-            newCategoryName.value = ''
-            newCategoryParentId.value = null
-        }
-    })
+function createCategory({ name, parentId, specsTypes = [] }: { name: string, parentId: number | null, specsTypes: any }) {
+    router.post('/admin/categories', { name, parentId, specsTypes })
 }
-
 
 function updateCategoryInline(rowIndex: number, item: CategoryDto) {
     router.put(`/admin/categories/${item.id}`, { name: item.name, parentId: item.parentId, specsTypes: item.specsTypes as any })
+}
+function deleteCategoryInline(item: CategoryDto) {
+    router.delete(`/admin/categories/${item.id}`)
 }
 </script>
 
 <template>
     <div>
         <h1 class="text-2xl font-bold mb-6">Admin Categories</h1>
-        <!-- Create Category Form -->
-
-        <!-- Categories Table -->
         <GenericTable :items="props.categories" :columns="[
             { key: 'id', label: 'ID', editable: false },
             { key: 'name', label: 'Name' },
             { key: 'parentId', label: 'Parent', type: 'select', options: props.categories },
             { key: 'specsTypes', label: 'Specs Types', type: 'array' }
         ]" @update:item="({ rowIndex, item }) => updateCategoryInline(rowIndex, item)"
-            @create:category="createCategory" />
+            @create:category="createCategory" @delete:item="deleteCategoryInline" />
 
     </div>
 </template>
