a73834a77a99f2274240136e48f8d72166abe81f
Lyrocs <cjuanole@gmail.com>
2025-08-29 11:33:01 +0200

    feat(admin): use grid on product admin list

--- inertia/pages/admin/products.vue
+++ inertia/pages/admin/products.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
 import AdminLayout from '~/layouts/AdminLayout.vue'
 import ProductDto from '#dtos/product'
-import { usePage, router } from '@inertiajs/vue3'
+import { router } from '@inertiajs/vue3'
 
 defineOptions({
     layout: AdminLayout
 })
 
 const props = defineProps<{ products: ProductDto[], meta: MetaDto, }>()
-const page = usePage()
 
 function handleChangePage(value: number) {
     const url = new URL(window.location.href)
@@ -20,20 +19,25 @@ function handleChangePage(value: number) {
 
 <template>
     <div>
-        <ul>
+        <ul class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
             <li v-for="product in props.products" :key="product.id" class="flex gap-4 border bg-white p-2 rounded mb-2">
                 <div class="flex items-center justify-center">
                     <img :src="product.images[0]" alt="" class="w-24 h-24 object-cover">
                 </div>
                 <div class="flex flex-col gap-2">
                     <h2 class="font-bold">{{ product.name }}</h2>
-                    <p>{{ product.brand.name }}</p>
-                    <p>{{ product.category.name }}</p>
+
+                    <p>{{ product.category.name }} - {{ product.brand.name }}</p>
+                    <ul class="flex gap-2">
+                        <li v-for="spec in product.specs" :key="spec.id">
+                            <Badge>{{ spec.type }}: {{ spec.value }}</Badge>
+                        </li>
+                    </ul>
                     <div class="flex gap-2">
                         <div>
                             <Badge :variant="product.status === 'PENDING' ? 'destructive' : 'secondary'">{{
                                 product.status
-                            }}
+                                }}
                             </Badge>
                         </div>
                         <div v-for="translation in product.translations" :key="translation.id" class="">
@@ -56,16 +60,12 @@ function handleChangePage(value: number) {
             :default-page="meta.currentPage">
             <PaginationContent v-slot="{ items }">
                 <PaginationPrevious />
-
                 <template v-for="(item, index) in items" :key="index">
                     <PaginationItem @click="handleChangePage(item.value)" v-if="item.type === 'page'"
                         :value="item.value" :is-active="item.value === page">
                         {{ item.value }}
                     </PaginationItem>
                 </template>
-
-                <!-- <PaginationEllipsis :index="4" /> -->
-
                 <PaginationNext />
             </PaginationContent>
         </Pagination>
